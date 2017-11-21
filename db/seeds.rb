6105d1711eb59a370953af4d7cee283a4d6b8d72
Ryan Rudat <ryanrudat@gmail.com>
2017-11-21 16:28:14 -0600

    cp16

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'random_data'
+
+50.times do
+
+  #we use create! with a bang (!). Adding a ! instructs the method to raise an error
+  #if there's a problem with the data we're seeding.
+  #Using create without a bang could fail without warning, causing the error to surface later.
+  Post.create!(
+
+  #we use methods from a class that does not exist yet, RandomData,
+  #that will create random strings for title and body.
+  #Writing code for classes and methods that don't exist is known as "wishful coding"
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph
+  )
+end
+posts = Post.all
+
+#we call times on an Integer (a number object).
+#This will run a given block the specified number of times, which is 100 in this case.
+#The end result of calling times is similar to that of a loop, but in this use-case,
+#it is easier to read and thus more idiomatic.
+100.times do
+  Comment.create!(
+
+#we call sample on the array returned by Post.all,
+#in order to pick a random post to associate each comment with. sample returns a random element from the array
+#every time it's called
+  post: posts.sample,
+  body: RandomData.random_paragraph
+  )
+end
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
