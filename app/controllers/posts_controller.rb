a24c3d918464b50e38dcc4b4b79c37352fec0b9a
Ryan Rudat <ryanrudat@gmail.com>
2017-11-27 13:14:29 -0600

    assignment17 completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,11 @@ class PostsController < ApplicationController
   def index
 
     @posts = Post.all
+    @post.each_with_index do |post, index|
+      if index % 5 == 0
+        post.title = "Spam!"
+      end
+    end
   end
 
   def show
